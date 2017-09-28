909b4e1390db5b2b99677968d4a480c7d98bd946
Haroun Ach <harounacho21@gmail.com>
2017-09-29 00:36:40 +0100

    applying gradient colors

--- app/src/main/java/com/havas/gradienttextview/MainActivity.java
+++ app/src/main/java/com/havas/gradienttextview/MainActivity.java
@@ -1,6 +1,9 @@
 package com.havas.gradienttextview;
 
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -17,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
         //setting custom font "Algerian"
         algerianFont = Typeface.createFromAsset(getAssets(), "ALGER.TTF");
         textView.setTypeface(algerianFont);
+
+        //applying gradient colors
+        int[] colors = {ContextCompat.getColor(this, R.color.font_color_a),
+                ContextCompat.getColor(this, R.color.font_color_b),
+                ContextCompat.getColor(this, R.color.font_color_c),
+                ContextCompat.getColor(this, R.color.font_color_d)};
+        float[] position = {0, 1, 2, 3};
+        Shader.TileMode tileMode = Shader.TileMode.MIRROR; // or TileMode.REPEAT;
+        LinearGradient linearGradient = new LinearGradient(0, 0, 0, 50,colors,position, tileMode);
+        Shader shaderGradient = linearGradient;
+        textView.getPaint().setShader(shaderGradient);
 
     }
 }
